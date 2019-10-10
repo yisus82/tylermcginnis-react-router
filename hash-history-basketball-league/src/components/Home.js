5a638685fd949d06fb415d51ba8c553d79de3e0f
Jesús Ángel Pérez-Roca Fernández <djalma8@gmail.com>
2019-10-10 16:27:27 +0200

    Setting up Home route

--- hash-history-basketball-league/src/components/Home.js
+++ hash-history-basketball-league/src/components/Home.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { getTeamNames } from '../api';
+import TeamLogo from './TeamLogo';
 
-const Home = () => <div className="container">HOME</div>;
+const Home = () => {
+  const [teamNames, setTeamNames] = React.useState([]);
+
+  React.useEffect(() => {
+    getTeamNames().then(newTeamNames => setTeamNames(() => newTeamNames));
+  }, [teamNames]);
+
+  return (
+    <div className="container">
+      <h1 className="large-header">Hash History Basketball League</h1>
+      <h3 className="header text-center">Select a team</h3>
+      <div className="home-grid">
+        {teamNames.map(id => (
+          <Link key={id} to={`/${id}`}>
+            <TeamLogo id={id} width="125px" />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
