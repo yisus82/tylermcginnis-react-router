d14db29906069ec5d6a0cc2d70b8ec626a7dd18a
yisus82 <djalma8@gmail.com>
2019-10-12 22:35:17 +0200

    Code splitting by route

--- hash-history-basketball-league/src/components/App.js
+++ hash-history-basketball-league/src/components/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './Home';
 import Navbar from './Navbar';
-import Players from './Players';
-import Teams from './Teams';
-import TeamPage from './TeamPage';
-import Articles from './Articles';
+import Loading from './Loading';
+
+const Home = React.lazy(() => import('./Home'));
+const Players = React.lazy(() => import('./Players'));
+const Teams = React.lazy(() => import('./Teams'));
+const TeamPage = React.lazy(() => import('./TeamPage'));
+const Articles = React.lazy(() => import('./Articles'));
 
 const App = () => (
   <Router>
     <div>
       <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/players" component={Players} />
-        <Route path="/teams" component={Teams} />
-        <Route path="/:teamId" exact component={TeamPage} />
-        <Route path="/:teamId/articles" component={Articles} />
-        <Route render={() => <h1 className="text-center">Four oh Four.</h1>} />
-      </Switch>
+      <React.Suspense fallback={<Loading />}>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/players" component={Players} />
+          <Route path="/teams" component={Teams} />
+          <Route path="/:teamId" exact component={TeamPage} />
+          <Route path="/:teamId/articles" component={Articles} />
+          <Route
+            render={() => <h1 className="text-center">Four oh Four.</h1>}
+          />
+        </Switch>
+      </React.Suspense>
     </div>
   </Router>
 );
